8c76c3b8f1d5c6dfbbf8f9a56186d960b59d820a
SimonOcampo1 <simontadeoocampo@alu.frlp.utn.edu.ar>
2025-10-02 17:04:16 -0300

    feat(routes): add register route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,12 @@ import { OfertasListaComponent } from './pages/ofertas-lista/ofertas-lista.compo
 import { OfertaDetalleComponent } from './pages/oferta-detalle/oferta-detalle.component';
 import { PerfilAlumnoComponent } from './pages/perfil-alumno/perfil-alumno.component';
 import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 export const routes: Routes = [
   { path: '', component: HomepageComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
   { path: 'ofertas', component: OfertasListaComponent },
   { path: 'oferta/:id', component: OfertaDetalleComponent },
   { path: 'perfil', component: PerfilAlumnoComponent }
