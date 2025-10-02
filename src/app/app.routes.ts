7c7bc3acddf2037fc812f7b3572890e297f19ca9
SimonOcampo1 <simontadeoocampo@alu.frlp.utn.edu.ar>
2025-10-02 16:43:52 -0300

    feat(routes): add login route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,13 +4,11 @@ import { OfertasListaComponent } from './pages/ofertas-lista/ofertas-lista.compo
 import { OfertaDetalleComponent } from './pages/oferta-detalle/oferta-detalle.component';
 import { PerfilAlumnoComponent } from './pages/perfil-alumno/perfil-alumno.component';
 import { LoginComponent } from './pages/login/login.component';
-import { AplicantesListaComponent } from './pages/aplicantes-lista/aplicantes-lista.component';
 
 export const routes: Routes = [
   { path: '', component: HomepageComponent },
   { path: 'login', component: LoginComponent },
   { path: 'ofertas', component: OfertasListaComponent },
   { path: 'oferta/:id', component: OfertaDetalleComponent },
-  { path: 'oferta/:id/aplicantes', component: AplicantesListaComponent },
   { path: 'perfil', component: PerfilAlumnoComponent }
 ];
