8c7b6670380be2a3e1291149095813b447fe936b
SimonOcampo1 <simontadeoocampo@alu.frlp.utn.edu.ar>
2025-10-02 16:02:18 -0300

    feat: add login route to application routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,14 @@ import { HomepageComponent } from './pages/homepage/homepage.component';
 import { OfertasListaComponent } from './pages/ofertas-lista/ofertas-lista.component';
 import { OfertaDetalleComponent } from './pages/oferta-detalle/oferta-detalle.component';
 import { PerfilAlumnoComponent } from './pages/perfil-alumno/perfil-alumno.component';
+import { LoginComponent } from './pages/login/login.component';
+import { AplicantesListaComponent } from './pages/aplicantes-lista/aplicantes-lista.component';
 
 export const routes: Routes = [
   { path: '', component: HomepageComponent },
+  { path: 'login', component: LoginComponent },
   { path: 'ofertas', component: OfertasListaComponent },
   { path: 'oferta/:id', component: OfertaDetalleComponent },
+  { path: 'oferta/:id/aplicantes', component: AplicantesListaComponent },
   { path: 'perfil', component: PerfilAlumnoComponent }
 ];
