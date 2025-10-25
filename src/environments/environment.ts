10c5f411afdb28b30fbc3549722499af1b9370bf
NachoL24 <ignaciolances24@gmail.com>
2025-10-25 02:59:32 -0300

    update environment configuration to prioritize local API URL for development

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-   apiUrl: 'https://api-unijobs.lafuah.com/api',
-  // apiUrl: 'http://localhost:8080/api',
+  // apiUrl: 'https://api-unijobs.lafuah.com/api',
+  apiUrl: 'http://localhost:8080/api',
 };
