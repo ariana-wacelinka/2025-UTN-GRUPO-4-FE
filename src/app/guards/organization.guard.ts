153e323a8960d7fcc08f964de3d496884c630a1f
NachoL24 <ignaciolances24@gmail.com>
2025-10-25 01:58:00 -0300

    add logging to organization guard for better debugging and user role verification

--- src/app/guards/organization.guard.ts
+++ src/app/guards/organization.guard.ts
@@ -6,6 +6,10 @@ export const organizationGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
   const router = inject(Router);
 
+  console.log('=== ORGANIZATION GUARD CALLED ===');
+  console.log('User is logged in: ', authService.isLoggedIn());
+  console.log('User logged: ', authService.keycloakUser);
+  console.log('User role: ', authService.keycloakUser?.role);
   if (authService.keycloakUser?.role === UserRole.ORGANIZATION) {
     return true;
   }
